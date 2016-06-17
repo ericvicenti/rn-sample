5a7f7fa5c91ef1a8cc0c715c23c94c7c47cb7ed5
John D. Storey <johndangerstorey@gmail.com>
2016-06-16 23:57:48 -0600

    Adds in dependencies, removes webdist, resolves case sensitivity

--- react-navigation/lib/NavigationHeaderBackButton.js
+++ react-navigation/lib/NavigationHeaderBackButton.js
@@ -11,12 +11,12 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * 
+ *
 */
 'use strict';
 
 var React=require('react');
-var ReactNative=require('react-native');var 
+var ReactNative=require('react-native');var
 
 
 Image=
--- webpack.config.js
+++ webpack.config.js
@@ -7,30 +7,22 @@
 
   module.exports = [
     {
-
       debug: true,
-
       // devtool: 'source-map',
-
       entry: {
         'server': ['./server.js'],
       },
-
       target: 'node',
-
       output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js',
       },
-
       resolve: {
         alias: {
           'react-native': 'react-native-web',
         },
       },
-
       module: {
-
         preLoaders: [
           // {
           //   test: /\.(js|jsx|es6)$/,
@@ -38,76 +30,6 @@
           //   loader: 'eslint-loader',
           // }
         ],
-
-        loaders: [
-          {
-            test: /\.js$/,
-            include: /node_modules\/react-native/,
-            loader: 'babel',
-            query: {
-              cacheDirectory: true,
-              presets: ['es2015', 'stage-1', 'react'],
-            }
-          },
-          {
-            test: /\.json$/,
-            // exclude: /node_modules/,
-            loader: 'json',
-          },
-          {
-            test: /\.(js|jsx|es6|json)$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-              cacheDirectory: true,
-              presets: ['es2015', 'stage-1', 'react'],
-            }
-          },
-          // {
-          //   test: /\.(gif|jpe?g|png|svg)$/,
-          //   loader: 'url-loader',
-          //   query: { name: '[name].[hash:16].[ext]' },
-          // },
-        ],
-      },
-    },
-    {
-
-      debug: true,
-
-      // devtool: 'source-map',
-
-      entry: {
-        'main.browser': ['./main.browser.js'],
-      },
-
-      output: {
-        path: path.resolve(__dirname, 'webdist'),
-        filename: '[name].js',
-      },
-
-      resolve: {
-        alias: {
-          'react-native': 'react-native-web',
-        },
-      },
-
-      plugins: [
-        new LiveReloadPlugin({
-          port: 9982,
-        }),
-      ],
-
-      module: {
-
-        preLoaders: [
-          // {
-          //   test: /\.(js|jsx|es6)$/,
-          //   include: path.resolve(__dirname, 'src'),
-          //   loader: 'eslint-loader',
-          // }
-        ],
-
         loaders: [
           {
             test: /\.js$/,
@@ -136,11 +58,9 @@
             test: /\.(gif|jpe?g|png|svg)$/,
             loader: 'url-loader',
             query: { name: '[name].[hash:16].[ext]' },
-          },
-        ],
-      },
-
+          }
+        ]
+      }
     }
   ];
-
 }())
